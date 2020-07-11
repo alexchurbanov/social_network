952630669e9161dd515008b502d4f5109610d1e8
Alexander <32096953+Red5ive007@users.noreply.github.com>
2020-07-11 21:02:07 +0300

    Filters change for Posts

--- Posts/serializers.py
+++ Posts/serializers.py
@@ -6,8 +6,6 @@ class PostSerializer(serializers.ModelSerializer):
     owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
     owner_username = serializers.CharField(source='owner.username', read_only=True)
     liked_by = serializers.StringRelatedField(many=True, read_only=True)
-    date_published = serializers.DateTimeField(format="%d-%m-%Y %H:%M %Z%z", read_only=True)
-    last_edit = serializers.DateTimeField(format="%d-%m-%Y %H:%M %Z%z", read_only=True)
 
     class Meta:
         model = Post
