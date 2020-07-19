95547b4f4c8870e4b8b19117936b3f60aead7208
Alexander <32096953+Red5ive007@users.noreply.github.com>
2020-07-19 15:59:38 +0300

    Update permissions.py

--- Accounts/permissions.py
+++ Accounts/permissions.py
@@ -7,13 +7,9 @@ class IsProfileOwnerOrAdmin(BasePermission):
             return True
 
         if 'pk' in view.kwargs.keys():
-<<<<<<< Updated upstream
-            if view.kwargs['pk'] == 'me':
-=======
             if request.user.is_anonymous:
                 return False
             if view.kwargs['pk'] == str(request.user.id) or view.kwargs['pk'] == 'me':
->>>>>>> Stashed changes
                 return True
             else:
                 return False
