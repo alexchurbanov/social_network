d94d30f63128f1a3399892f270f67393fbb831aa
Alexander <32096953+Red5ive007@users.noreply.github.com>
2020-07-30 16:17:52 +0300

    Analytics filter backend added

--- Posts/filters.py
+++ Posts/filters.py
@@ -1,4 +1,6 @@
 from django_filters import rest_framework as filters
+from rest_framework.filters import BaseFilterBackend
+
 from .models import Post
 
 
@@ -11,3 +13,18 @@ class PostsFilter(filters.FilterSet):
     class Meta:
         model = Post
         fields = ('date_published_from', 'date_published_to', 'author_username', 'liked_by')
+
+
+class DateRangePostLikesFilter(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        date_from = request.query_params.get('date_from', None)
+        date_to = request.query_params.get('date_to', None)
+        if date_from and date_to:
+            queryset = queryset.filter(created__gte=date_from,
+                                       created__lte=date_to)
+        elif date_from:
+            queryset = queryset.filter(created__gte=date_from)
+        elif date_to:
+            queryset = queryset.filter(created__lte=date_to)
+
+        return queryset
